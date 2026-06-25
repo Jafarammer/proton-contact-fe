7ed2fe4adcdc2df39e8bacc6a1dc961e45d35152
Your Name <jafalkaff96@gmail.com>
2026-06-25 22:00:56 +0700

    add page register

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,125 @@
 import React from "react";
+import { Typography, Card, Flex, Form, Input, Button, Divider } from "antd";
+import { FaUser, FaLock } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
+import { IoShieldCheckmark } from "react-icons/io5";
+import { Link } from "react-router-dom";
+
+const { Title, Text } = Typography;
+type FieldType = {
+  fullName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register-container">
+      <Title level={3} className="title">
+        Proton Contact
+      </Title>
+      <Text type="secondary">Efficiency. Reliabillity. Precision.</Text>
+      <div>
+        <Card className="card">
+          <Flex vertical align="flex-start">
+            <Title level={4} className="title">
+              Create an account
+            </Title>
+            <Text type="secondary">
+              Manage your profesional network with calm control.
+            </Text>
+          </Flex>
+          <Form
+            layout="vertical"
+            name="formRegister"
+            autoComplete="off"
+            className="form"
+          >
+            <Form.Item<FieldType>
+              label="Full Name"
+              name="fullName"
+              rules={[{ required: true, message: "Full Name is required!" }]}
+              required={false}
+            >
+              <Input
+                size="large"
+                name="fullName"
+                type="text"
+                autoComplete="off"
+                prefix={<FaUser size={16} />}
+              />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              label="Email Address"
+              name="email"
+              rules={[{ required: true, message: "Email is required!" }]}
+              required={false}
+            >
+              <Input
+                size="large"
+                name="email"
+                type="email"
+                autoComplete="off"
+                prefix={<IoMail size={16} />}
+              />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: "Password is required!" }]}
+              required={false}
+            >
+              <Input.Password
+                size="large"
+                name="password"
+                autoComplete="off"
+                prefix={<FaLock size={16} />}
+              />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+              label="Confirm Password"
+              name="confirmPassword"
+              dependencies={["password"]}
+              rules={[
+                { required: true, message: "Please confirm your password!" },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+
+                    return Promise.reject(
+                      new Error("The passwords do not match!"),
+                    );
+                  },
+                }),
+              ]}
+              required={false}
+            >
+              <Input.Password
+                size="large"
+                name="confirmPassword"
+                autoComplete="off"
+                prefix={<IoShieldCheckmark size={16} />}
+              />
+            </Form.Item>
+
+            <Button htmlType="submit" block type="primary" size="large">
+              Register
+            </Button>
+          </Form>
+        </Card>
+        <Divider />
+      </div>
+      <Text type="secondary">
+        Already have an account ? <Link to={"/"}>Log in</Link>
+      </Text>
+    </div>
+  );
 };
 
 export default Register;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { FormProps } from "antd";
-import { Typography, Card, Form, Input, Button } from "antd";
+import { Typography, Card, Form, Input, Button, Divider } from "antd";
 import { BiSolidUserRectangle } from "react-icons/bi";
 import { FaLock } from "react-icons/fa";
 import { FaArrowRightToBracket } from "react-icons/fa6";
@@ -23,51 +23,58 @@ const Login = () => {
         Proton Contact
       </Title>
       <Text type="secondary">Scure precision for modern profesionals.</Text>
-      <Card className="card">
-        <Form layout="vertical" name="formLogin" autoComplete="off">
-          <Form.Item<FieldType>
-            label="Email Address"
-            name="email"
-            rules={[{ required: true, message: "Please input your email!" }]}
-            required={false}
-          >
-            <Input
-              size="large"
-              type="email"
+      <div>
+        <Card className="card">
+          <Form layout="vertical" name="formLogin" autoComplete="off">
+            <Form.Item<FieldType>
+              label="Email Address"
               name="email"
-              prefix={<IoMail size={20} />}
-            />
-          </Form.Item>
+              rules={[{ required: true, message: "Please input your email!" }]}
+              required={false}
+            >
+              <Input
+                size="large"
+                type="email"
+                name="email"
+                prefix={<IoMail size={16} />}
+                autoComplete="off"
+              />
+            </Form.Item>
 
-          <Form.Item<FieldType>
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-            required={false}
-          >
-            <Input.Password
+            <Form.Item<FieldType>
+              label="Password"
               name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+              required={false}
+            >
+              <Input.Password
+                name="password"
+                size="large"
+                prefix={<FaLock size={16} />}
+                autoComplete="off"
+              />
+            </Form.Item>
+
+            <Link to="/forgot-password" className="link">
+              Forgot Password?
+            </Link>
+
+            <Button
+              htmlType="submit"
+              block
+              type="primary"
               size="large"
-              prefix={<FaLock size={20} />}
-            />
-          </Form.Item>
-
-          <Link to="/forgot-password" className="link">
-            Forgot Password?
-          </Link>
-
-          <Button
-            htmlType="submit"
-            block
-            type="primary"
-            size="large"
-            icon={<FaArrowRightToBracket aria-label="icon-arrow" />}
-            iconPosition="end"
-          >
-            Sign In to Dashboard
-          </Button>
-        </Form>
-      </Card>
+              icon={<FaArrowRightToBracket aria-label="icon-arrow" />}
+              iconPosition="end"
+            >
+              Sign In to Dashboard
+            </Button>
+          </Form>
+        </Card>
+        <Divider />
+      </div>
       <Text type="secondary">
         New to Proton Contact ? <Link to={"/register"}>Create an account</Link>
       </Text>
